522516753d91031f488a4cf05b3feafff41bdc7e
verissimo-sn <jose.neto@minu.co>
2022-12-27 14:08:44 -0300

    feat: added product sequelize implementation

---
+++ src/infra/repository/product.repositpry.ts
@@ -0,0 +1,42 @@
+import { Product } from '@domain/entity/product';
+import { IProductRepository } from '@domain/repository/product-repository.interface';
+import { ProductModel } from '@infra/db/sequelize/model/product.model';
+
+export class ProductRepository implements IProductRepository {
+  async create(entity: Product): Promise<void> {
+    await ProductModel.create({
+      id: entity.id,
+      name: entity.name,
+      price: entity.price,
+    });
+  }
+
+  async update(entity: Product): Promise<void> {
+    await ProductModel.update(
+      {
+        name: entity.name,
+        price: entity.price,
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
+  }
+
+  async find(id: string): Promise<Product | null> {
+    const productModel = await ProductModel.findOne({ where: { id } });
+    return productModel
+      ? new Product(productModel.id, productModel.name, productModel.price)
+      : null;
+  }
+
+  async findAll(): Promise<Product[]> {
+    const productModel = await ProductModel.findAll();
+
+    return productModel.map((product) => {
+      return new Product(product.id, product.name, product.price);
+    });
+  }
+}
