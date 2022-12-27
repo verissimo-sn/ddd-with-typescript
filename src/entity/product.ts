b22b5a430de9885ae9363406c9c601893343584c
verissimo-sn <jose.neto@minu.co>
2022-12-26 21:36:40 -0300

    feat: added product entity

---
+++ src/entity/product.ts
@@ -0,0 +1,43 @@
+export class Product {
+  private _id: string;
+  private _name: string;
+  private _price: number;
+
+  constructor(id: string, name: string, price: number) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+    this.validate();
+  }
+
+  validate() {
+    if (!this._id.length) {
+      throw new Error('Id is required');
+    }
+
+    if (!this._name.length) {
+      throw new Error('Name is required');
+    }
+
+    if (this._price <= 0) {
+      throw new Error('Price is greater than 0');
+    }
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  get price() {
+    return this._price;
+  }
+}
