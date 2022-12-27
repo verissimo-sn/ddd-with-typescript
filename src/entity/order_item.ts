adb70a2ad7fb0d5b0dc0c551f7dde7e48a07e7e0
verissimo-sn <jose.neto@minu.co>
2022-12-26 22:01:40 -0300

    feat: added product relation

--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,12 +1,24 @@
 export class OrderItem {
   private _id: string;
+  private _productId: string;
   private _name: string;
   private _price: number;
+  private _quantity: number;
 
-  constructor(id: string, name: string, price: number) {
+  constructor(
+    id: string,
+    name: string,
+    productId: string,
+    price: number,
+    quantity: number
+  ) {
     this._id = id;
     this._name = name;
+    this._productId = productId;
     this._price = price;
+    this._quantity = quantity;
+
+    this.validate();
   }
 
   validate() {
@@ -18,8 +30,16 @@ export class OrderItem {
       throw new Error('Name is required');
     }
 
-    if (!this._price) {
-      throw new Error('Price is required');
+    if (!this._productId) {
+      throw new Error('ProductId is required');
+    }
+
+    if (this._price <= 0) {
+      throw new Error('Price should greater then 0');
+    }
+
+    if (this._quantity <= 0) {
+      throw new Error('Quantity should greater then 0');
     }
   }
 
@@ -31,7 +51,11 @@ export class OrderItem {
     return this._name;
   }
 
+  get productId() {
+    return this._productId;
+  }
+
   get price() {
-    return this._price;
+    return this._price * this._quantity;
   }
 }
