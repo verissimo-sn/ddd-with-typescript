ecc7091508c42ab9591c3798bdfdd5949238f669
verissimo-sn <jose.neto@minu.co>
2022-12-26 23:30:31 -0300

    feat: added place an order and add reward points

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,9 +1,24 @@
+import { randomUUID } from 'crypto';
+
+import { Customer } from '../entity/customer';
 import { Order } from '../entity/order';
+import { OrderItem } from '../entity/order_item';
 
 export class OrderService {
   static total(orders: Order[]): number {
     return orders.reduce((acc, order) => {
       return acc + order.total();
     }, 0);
+  }
+
+  static placeOrder(customer: Customer, items: OrderItem[]): Order {
+    if (!items.length) {
+      throw new Error('Order must have at list of items');
+    }
+
+    const id = randomUUID();
+    const order = new Order(id, customer.id, items);
+    customer.addRewardPoints(order.total() / 2);
+    return order;
   }
 }
