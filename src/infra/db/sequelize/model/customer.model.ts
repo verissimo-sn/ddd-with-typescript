fb6f678620b2c55a4b18d2e2e3413b8f421ec075
verissimo-sn <jose.neto@minu.co>
2022-12-28 16:24:28 -0300

    feat: added customer model

---
+++ src/infra/db/sequelize/model/customer.model.ts
@@ -0,0 +1,35 @@
+import { Column, Model, PrimaryKey, Table } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'customers',
+  timestamps: false,
+})
+export class CustomerModel extends Model {
+  @PrimaryKey
+  @Column
+  declare id: string;
+
+  @Column({ allowNull: false })
+  declare name: string;
+
+  @Column({ allowNull: false })
+  declare street: string;
+
+  @Column({ allowNull: false })
+  declare number: number;
+
+  @Column({ allowNull: false })
+  declare zipCode: number;
+
+  @Column({ allowNull: false })
+  declare city: string;
+
+  @Column({ allowNull: false })
+  declare country: string;
+
+  @Column({ allowNull: false })
+  declare status: boolean;
+
+  @Column({ allowNull: false })
+  declare rewardPoints: number;
+}
