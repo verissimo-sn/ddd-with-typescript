2435ff69ac0e94d70cacb4e4c12fd8872479e8dd
verissimo-sn <jose.neto@minu.co>
2022-12-31 18:05:10 -0300

    feat: added send email handler

---
+++ src/domain/event/product/handler/send-email-when-product-is-created.handler.ts
@@ -0,0 +1,10 @@
+import { IEventHandler } from '../../@shared/event-handler.interface';
+import { ProductCreatedEvent } from '../product-created.event';
+
+export class SendEmailWhenProductIsCreatedHandler
+  implements IEventHandler<ProductCreatedEvent>
+{
+  handle(event: ProductCreatedEvent): void {
+    console.log('Sending email');
+  }
+}
